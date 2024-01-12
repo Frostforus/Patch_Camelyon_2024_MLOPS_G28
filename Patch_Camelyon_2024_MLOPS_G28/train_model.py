601b925da7e42b22d1bf5270c610b245fab7de58
kernibeni15 <kernibeni17@gmail.com>
2024-01-12 22:37:06 +0100

    config_file

--- Patch_Camelyon_2024_MLOPS_G28/train_model.py
+++ Patch_Camelyon_2024_MLOPS_G28/train_model.py
@@ -1,13 +1,18 @@
 from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import WandbLogger
 from torchvision.datasets import PCAM
+import hydra
 
 from models.model import SimpleCNN
 
-def train():
+@hydra.main(config_path="conf", config_name="config")
+def train(cfg):
     model = SimpleCNN()
     wandb_logger = WandbLogger(log_model=True,project='Patch_Camelyon_MLOps_WandB')
-    trainer = Trainer(accelerator='gpu', logger=wandb_logger, max_epochs=50, limit_train_batches=0.5, check_val_every_n_epoch=2)
+    trainer = Trainer(accelerator=cfg.trainer.accelerator, logger=wandb_logger,
+        max_epochs=cfg.trainer.max_epochs,
+        limit_train_batches=cfg.trainer.limit_train_batches,
+        check_val_every_n_epoch=cfg.trainer.check_val_every_n_epoch)
     trainer.fit(model)
 
 if __name__ == '__main__':
