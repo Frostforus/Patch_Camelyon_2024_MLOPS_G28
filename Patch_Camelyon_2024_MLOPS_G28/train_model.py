77a1f7217ccdf6b0ed2d6758b4171f4599458df9
MarioRojoVicente <estudiantedinamo@gmail.com>
2024-01-15 09:58:00 +0100

    Revert "moving dvc to data folder"
    This reverts commit e6a9e09af1ba8f01b9e117a45db34d7a746b6feb.

--- Patch_Camelyon_2024_MLOPS_G28/train_model.py
+++ Patch_Camelyon_2024_MLOPS_G28/train_model.py
@@ -1,6 +1,5 @@
 from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import WandbLogger
-import torch
 
 from models.model import SimpleCNN
 
@@ -8,12 +7,8 @@ from models.model import SimpleCNN
 def train():
     model = SimpleCNN()
     wandb_logger = WandbLogger(log_model=True, project="Patch_Camelyon_MLOps_WandB")
-    if torch.cuda.is_available():
-        accelerator = "gpu"
-    else:
-        accelerator = "cpu"
     trainer = Trainer(
-        accelerator=accelerator, logger=wandb_logger, max_epochs=50, limit_train_batches=0.5, check_val_every_n_epoch=2
+        accelerator="gpu", logger=wandb_logger, max_epochs=50, limit_train_batches=0.5, check_val_every_n_epoch=2
     )
     trainer.fit(model)
 
