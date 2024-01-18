c438793a66983632daa7c2ba15329a64b2fdebb8
drexler-konrad <kdr@healper.dk>
2024-01-18 14:39:56 +0100

    Needs loading to work properly.

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,3 +1,4 @@
+import torch
 import yaml
 from fastapi import FastAPI, UploadFile, File
 from http import HTTPStatus
@@ -10,7 +11,6 @@ import numpy as np
 
 class PredictionModel:
     def __init__(self,
-                 force_download=False,
                  yaml_file_path='./config/config.yaml'
                  ):
         self.model = None
@@ -20,50 +20,54 @@ class PredictionModel:
             configs = yaml.safe_load(file)
             self.source_bucket_name = configs['google_cloud']['bucket_name']
             self.source_model_blob_name = configs['google_cloud']['model_blob_name']
-            self.destination_file_name = configs['local_model_file_name']
+            self.destination_dir_path = configs['local']['model_dir']
+            self.destination_file_name = configs['local']['model_file_name']
+            self.destination_file_path = self.destination_dir_path + '/' + self.destination_file_name
             self.key_file_path = configs['google_cloud']['key_file_name']
-            self.target_size = tuple((configs['target_size']['height'], configs['target_size']['width']))
+            self.model_input_dimensions = {"height": configs['model']['input_dimensions']['height'],
+                                           "width": configs['model']['input_dimensions']['width'],
+                                           "channels": configs['model']['input_dimensions']['channels']}
 
+        self._load_model(configs['model']['force_download'])
 
-        self.model = self._load_model(force_download)
-
-    # TODO: add parameters to init function too
     def _download_model_from_blob(self):
         """Downloads a blob from the bucket."""
         storage_client = storage.Client.from_service_account_json(self.key_file_path)
         bucket = storage_client.get_bucket(self.source_bucket_name)
         blob = bucket.blob(self.source_model_blob_name)
 
-        print(f"Started download of {self.source_model_blob_name} to {self.destination_file_name}.")
-        blob.download_to_filename(self.destination_file_name)
-        print(f"Model {self.source_model_blob_name} downloaded to {self.destination_file_name}.")
+        print(f"Started download of {self.source_model_blob_name} to {self.destination_file_path}.")
+        blob.download_to_filename(self.destination_dir_path + '/' + self.destination_file_name)
+        print(f"Model {self.source_model_blob_name} downloaded to {self.destination_file_path}.")
 
-    def _load_model(self, force_download):
-        # List model files in the current directory
-        files = [model_file_name for model_file_name in os.listdir(".") if model_file_name.endswith(".ckpt")]
-        print(files)
+    def _load_model(self, force_download=False):
+        # List model files in the current directory,
+        files = [self.destination_dir_path + '/' + model_file_name for model_file_name in
+                 os.listdir(self.destination_dir_path) if model_file_name.endswith(".pth")]
         if len(files) == 0 or force_download:
             print("No local model found. Downloading model from GCS.")
             self._download_model_from_blob()
-
-            files.append(self.destination_file_name)
-
         else:
-            print(f"Local model found, loading from file: {files[0]}")
+            print(f"Local model found, loading from file: {self.destination_file_path}")
 
-        return files[0]
+        # Load the model from local file, and enable eval mode, to disable randomness and dropout
+        self.model = torch.load(self.destination_file_path)
+        self.model.eval()
 
     def _resize_image(self, image: UploadFile):
-
         contents = image.file.read()
         image_array = cv2.imdecode(np.frombuffer(contents, np.uint8), cv2.IMREAD_COLOR)
-        return cv2.resize(image_array, self.target_size)
+        return cv2.resize(image_array, self.model_input_dimensions)
 
-
-    def predict(self, image: UploadFile=None):
+    def predict(self, image: UploadFile = None):
         if image:
-            self._resize_image(image)
-        return "Prediction with " + self.model
+            image = self._resize_image(image)
+            # Convert image to tensor and add batch dimension,
+            #TODO: swtich this with the correct predict function
+            prediction = self.model.predict(image)
+            return prediction
+        else:
+            return "No image provided."
 
 
 # Global variables,
@@ -79,7 +83,11 @@ async def prediction_test():
 
 @app.post("/predict/")
 async def predict_from_image(image: UploadFile = File(...)):
-    print("File uploaded, proceeding with prediction")
-    msg = prediction_model.predict(image)
+    try:
+        print("File uploaded, proceeding with prediction")
+        msg = prediction_model.predict(image)
 
-    return {"message": "Testing with uploaded image. " + msg}
+        return {"message": "Prediction result: " + msg, "prediction": msg, "status": HTTPStatus.OK}
+    except Exception as e:
+        print("Error occurred during prediction: ", e)
+        return {"message": "Error occurred during prediction: " + str(e), "status": HTTPStatus.IM_A_TEAPOT}
